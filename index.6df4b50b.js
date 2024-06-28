d67fc7d32ebe91e2761ea77f1233251a732742f0
Misha Vynnyk <vinnikmisha94@gmail.com>
2024-06-28 09:09:48 +0200

    make build

---
+++ index.6df4b50b.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in t)if(t.hasOwnProperty(a)){var l=document.createElement("li");l.textContent=a,r.appendChild(l),Object.keys(t[a]).length>0&&e(l,t[a])}n.appendChild(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.6df4b50b.js.map
